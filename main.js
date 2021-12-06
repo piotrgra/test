0f5b722d47ff2a1599e7cad720f3c6aeb4635c11
Piotr <grabowskipiotr1989@gmail.com>
2021-12-06 14:17:47 +0100

    test

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-let about = await fetch('_posts/2021-11-29-new-blog.md')
-let message = await about.json()
 
-console.log('mes',message);
+console.log('test');
+(async function () {
+    let about = await fetch('_posts/2021-11-29-new-blog.md')
+    let message = await about;
+    
+    console.log('mes',message);
+})()
